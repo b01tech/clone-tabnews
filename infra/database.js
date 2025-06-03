8bc5843d003ff5c68ee4b7c5130ec27118df6aec
b01tech <bruno01.tech@gmail.com>
2025-06-03 16:24:37 -0300

    adiciona arquivo `docker-compose` e `.env`

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,19 @@
 import { Client } from "pg";
 
 async function query(queryObj) {
-    const client = new Client();
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+    });
+    console.log("===DEBUGGER===");
+    console.log(`host: ${client.host}`);
+    console.log(`port: ${client.port}`);
+    console.log(`user: ${client.user}`);
+    console.log(`database: ${client.database}`);
+    console.log(`password: ${client.password}`);
     await client.connect();
     const result = await client.query(queryObj);
     await client.end();
