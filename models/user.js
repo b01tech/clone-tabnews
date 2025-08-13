b6dd0afacc94ded817fd01452a3f826689800a2c
b01tech <bruno01.tech@gmail.com>
2025-08-13 20:50:17 -0300

    feat: adiciona validacao do model user

--- models/user.js
+++ models/user.js
@@ -1,17 +1,63 @@
-import database from "infra/database";
+import database from "infra/database.js";
+import { ValidationError } from "infra/errors/errors.js";
 
 async function create(userInputValues) {
-    const result = await database.query({
-        text: `INSERT INTO users (username, email, password)
-           VALUES ($1, $2, $3)
-           RETURNING *`,
-        values: [
-            userInputValues.username,
-            userInputValues.email,
-            userInputValues.password,
-        ],
-    });
-    return result.rows[0];
+    await validateUserName(userInputValues.username);
+    await validateEmail(userInputValues.email);
+    const newUser = await runInsertQuery(userInputValues);
+    return newUser;
+
+    async function validateUserName(username) {
+        if (username == "") {
+            throw new ValidationError({
+                message: "Nome de usuário é obrigatório",
+                action: "Utilize outro nome de usuário",
+            });
+        }
+        if (username.length < 3) {
+            throw new ValidationError({
+                message: "Nome de usuário deve ter pelo menos 3 caracteres",
+                action: "Utilize outro nome de usuário",
+            });
+        }
+
+        const result = await database.query({
+            text: `SELECT username FROM users WHERE LOWER(username) = LOWER($1)`,
+            values: [username],
+        });
+        if (result.rows.length > 0) {
+            throw new ValidationError({
+                message: "Nome de usuário já cadastrado",
+                action: "Utilize outro nome de usuário",
+            });
+        }
+    }
+    async function validateEmail(email) {
+        const result = await database.query({
+            text: `SELECT email FROM users WHERE LOWER(email) = LOWER($1)`,
+            values: [email],
+        });
+        if (result.rows.length > 0) {
+            throw new ValidationError({
+                message: "Email já cadastrado",
+                action: "Utilize outro email",
+            });
+        }
+    }
+
+    async function runInsertQuery(userInputValues) {
+        const result = await database.query({
+            text: `INSERT INTO users (username, email, password)
+               VALUES ($1, $2, $3)
+               RETURNING *`,
+            values: [
+                userInputValues.username,
+                userInputValues.email,
+                userInputValues.password,
+            ],
+        });
+        return result.rows[0];
+    }
 }
 
 const user = {
--- tests/api/v1/users/post.test.js
+++ tests/api/v1/users/post.test.js
@@ -35,4 +35,116 @@ describe("POST api/v1/users", () => {
         expect(Date.parse(responseBody.created_at)).not.toBeNaN();
         expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
     });
+
+    test("Create a user with email already registered should returns 400", async () => {
+        const response1 = await fetch("http://localhost:3000/api/v1/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: "Jane Doe",
+                email: "jane.doe@example.com",
+                password: "XXXXXXXXXXX",
+            }),
+        });
+        expect(response1.status).toBe(201);
+        const response2 = await fetch("http://localhost:3000/api/v1/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: "Jane Doe2",
+                email: "Jane.Doe@example.com",
+                password: "XXXXXXXXXXX",
+            }),
+        });
+        const responseBody2 = await response2.json();
+        expect(response2.status).toBe(400);
+        expect(responseBody2).toEqual({
+            name: "ValidationError",
+            message: "Email já cadastrado",
+            action: "Utilize outro email",
+            status_code: 400,
+        });
+    });
+
+    test("Create a user with username already registered should returns 400", async () => {
+        const response1 = await fetch("http://localhost:3000/api/v1/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: "Alice Doe",
+                email: "alice.doe@example.com",
+                password: "XXXXXXXXXXX",
+            }),
+        });
+        expect(response1.status).toBe(201);
+        const response2 = await fetch("http://localhost:3000/api/v1/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: "Alice Doe",
+                email: "alice.doe2@example.com",
+                password: "XXXXXXXXXXX",
+            }),
+        });
+        const responseBody2 = await response2.json();
+        expect(response2.status).toBe(400);
+        expect(responseBody2).toEqual({
+            name: "ValidationError",
+            message: "Nome de usuário já cadastrado",
+            action: "Utilize outro nome de usuário",
+            status_code: 400,
+        });
+    });
+
+    test("Create a user with username empty should returns 400", async () => {
+        const response = await fetch("http://localhost:3000/api/v1/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: "",
+                email: "empty@example.com",
+                password: "XXXXXXXXXXX",
+            }),
+        });
+        const responseBody = await response.json();
+        expect(response.status).toBe(400);
+        expect(responseBody).toEqual({
+            name: "ValidationError",
+            message: "Nome de usuário é obrigatório",
+            action: "Utilize outro nome de usuário",
+            status_code: 400,
+        });
+    });
+
+    test("Create a user with username with less than 3 characters should returns 400", async () => {
+        const response = await fetch("http://localhost:3000/api/v1/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: "ab",
+                email: "ab@example.com",
+                password: "XXXXXXXXXXX",
+            }),
+        });
+        const responseBody = await response.json();
+        expect(response.status).toBe(400);
+        expect(responseBody).toEqual({
+            name: "ValidationError",
+            message: "Nome de usuário deve ter pelo menos 3 caracteres",
+            action: "Utilize outro nome de usuário",
+            status_code: 400,
+        });
+    });
 });
