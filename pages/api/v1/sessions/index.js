f33e4b78cccad0b2960253e19718c272b474c602
b01tech <bruno01.tech@gmail.com>
2026-02-06 15:12:33 -0300

    refactor(auth): extract authentication logic to dedicated model

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,8 +1,6 @@
 import controller from "infra/controller.js";
 import { createRouter } from "next-connect";
-import user from "models/user.js";
-import password from "models/password.js";
-import { UnauthorizedError } from "infra/errors/errors.js";
+import authentication from "models/authentication.js";
 
 const router = createRouter();
 
@@ -12,22 +10,9 @@ export default router.handler(controller.errorHandlers);
 
 async function postHandler(request, response) {
     const loginInputValues = request.body;
-    try {
-        const storedUser = await user.findByEmail(loginInputValues.email);
-        const isPasswordCorrect = await password.compare(
-            loginInputValues.password,
-            storedUser.password,
-        );
-
-        if (!isPasswordCorrect) {
-            throw new UnauthorizedError("Senha incorreta");
-        }
-    } catch (error) {
-        throw new UnauthorizedError({
-            message: "Email ou senha incorretos",
-            action: "Verifique suas credenciais e tente novamente",
-        });
-    }
+    const authenticatedUser =
+        await authentication.getAuthenticatedUser(loginInputValues);
+    console.log(authenticatedUser);
 
     return response.status(201).json({ message: "Session created" });
 }
